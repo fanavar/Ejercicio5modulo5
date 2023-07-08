ff1a6e3b4766d4451f4a41d246862f2d0a7d7073
Fabián <fanavar@alumnos.ubiobio.cl>
2023-07-07 21:59:57 -0400

    Ejercicio 5 constraintLayout + recyclerView + navigation + metodos del Adaptador

---
+++ app/src/main/java/cl/awakelab/ejercicio5/AdapterWords.java
@@ -0,0 +1,47 @@
+package cl.awakelab.ejercicio5;
+
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.progressindicator.BaseProgressIndicator;
+
+import java.util.List;
+
+import cl.awakelab.ejercicio5.databinding.ItemBinding;
+
+public class AdapterWords extends RecyclerView.Adapter <AdapterWords.ViewHolder>{
+    private List<String> words;
+    @NonNull
+    @Override
+    public AdapterWords.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        ItemBinding binding = ItemBinding.inflate(LayoutInflater.from(parent.getContext()));
+        return new ViewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AdapterWords.ViewHolder holder, int position) {
+        String item = words.get(position);
+        holder.showInformation(item);
+    }
+
+    @Override
+    public int getItemCount() {
+        return words.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private ItemBinding itemBinding;
+        public ViewHolder(@NonNull ItemBinding binding) {
+            super(binding.getRoot());
+            itemBinding = binding;
+        }
+        public void showInformation(String words){
+            itemBinding.textView.setText(words);
+        }
+    }
+}
